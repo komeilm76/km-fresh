7742162305d2c0bf9b41fa31e28c471b15e75329
komeilm76 <komeilmohammadian1376.web@gmail.com>
2024-10-15 01:49:22 +0330

    change export mode

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import lib from './lib';
 const kmFresh = lib.service.fresh;
+export const ref = kmFresh.ref;
+export const computed = kmFresh.computed;
+export const reactive = kmFresh.reactive;
 export default kmFresh;
